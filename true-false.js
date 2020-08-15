6bc2f89e0a3f0c761b222c241962b050b1cf4d06
wasifmislam <67533737+wasifmislam@users.noreply.github.com>
2020-08-16 02:17:27 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,25 @@
+// beside 0 all values (numbers) are taken as true in javaScript (4,-4 considered as true but 0 considered as false)
+//const age = 4;
+
+// if (age > 0){
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+// //all strings are true other than an empty string
+
+// //const name = " "
+// if (name) {
+//     console.log("it is true");
+// } else {
+//     console.log("it is false");
+// }
+// any undefined value considered as a false in js
+// 0 , "" , undefined, null, NaN , false all of these are false;
+// "0", " ", {}, [] these are true
+let name = false;
+if (name) {
+    console.log("it is true");
+} else {
+    console.log("it is false");
+}
