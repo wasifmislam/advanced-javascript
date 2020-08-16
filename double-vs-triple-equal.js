e1be4d1b187bfd362133d15436b0719fb0333251
wasifmislam <67533737+wasifmislam@users.noreply.github.com>
2020-08-16 19:31:21 +0600

    double equal vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,18 @@
+// const first = 2;
+// const second = "2";
+// if(first===second) {
+//     console.log("condition is true")
+// } else {
+//     console.log("condition is false");
+// }
+
+// == only check value. === check valu and type both
+//js compares true with 1, and false with 0; for ==
+
+const first = 1;
+const second = true;
+if(first==second) {
+    console.log("condition is true")
+} else {
+    console.log("condition is false");
+}
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -3,10 +3,12 @@ function add(num1, num2){
     console.log(num1+num2);
     return;
 }
+
 function add (num1, num2) {
     console.log(num1,num2);
 }
 const result = add(55);
+
 //you are accessing something which was not exist will give you undefied
 const jobHolder = {name:"jonson", age:35};
 console.log(jobHolder.adress);
